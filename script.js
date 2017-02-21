1ff0530fedacafc91be30fb70b05378d7a7372df
helenie-z <helen.jiahe.zhao@gmail.com>
2017-02-21 00:33:12 -0800

    updated background, changed some style

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 var finalDate = 'February 28 2017 6:00:00 GMT+1300';
 var anniversary = 'December 2 2016 1:00:00 GMT+1300';
+var alertDone = false;
 //get difference 
-function timeDifference(finalDate) {
+function timeDifference(date) {
 	//difference in milliseconds
 	var currentDate = new Date();
 
-	var difference = Date.parse(finalDate) - Date.parse(currentDate);
+	var difference = Date.parse(date) - Date.parse(currentDate);
 
 	var seconds = difference / 1000;
-	var leftOverSeconds = Math.floor(seconds % 60);
+	var leftOverSeconds = Math.abs(Math.floor(seconds % 60));
+	//alert(leftOverSeconds + 1);
 
 	var minutes = seconds / 60;
-	var leftOverMinutes = Math.floor(minutes % 60);
+	var leftOverMinutes = Math.abs(Math.floor(minutes % 60));
 
 	var hours = minutes / 60;
-	var leftOverHours = Math.floor(hours % 24);
+	var leftOverHours = Math.abs(Math.floor(hours % 24));
 
-	var days = Math.floor(hours / 24);
+	var days = Math.abs(Math.floor(hours / 24));
+	alertDay = days;
 
 	return {
 		'total': difference,
@@ -30,33 +33,38 @@ function timeDifference(finalDate) {
 //display difference
 function initializeClock(id, finalDate){
   var clock = document.getElementById("clock");
+
   var timeinterval = setInterval(function(){
     var timeRemaining = timeDifference(finalDate);
     var anniversaryTimeRemaining = timeDifference(anniversary);
 
+    if (!alertDone) {
+    	alert("OMG ONLY " + timeRemaining.days + " DAYS AND I CAN'T WAIT TO SEE YOU");
+    	alertDone = true;
+    }
+
     clock.innerHTML = 'hey mark <br>' + "<br>" + 
-    					'there\'s only <br>' +
-    					timeRemaining.days + ' days<br>' +
+    					'there\'s only ' + 
+    					Math.abs(timeRemaining.days) + ' days <br>' +
                       timeRemaining.hours + ' hours <br>' +
                       timeRemaining.minutes + ' minutes <br>' +
-                      'and ' + timeRemaining.seconds +' seconds <br>' + 
-                      'until i see you again :) <br>' + 
-  						'<br>ALSO<br>' +
-  							anniversaryTimeRemaining.days + ' days<br>' +
+                      'and ' + timeRemaining.seconds +' seconds until i see you again :) <br>' + '<br>' + 
+                      
+  						'and ' +
+  						anniversaryTimeRemaining.days + ' days<br>' +
                       anniversaryTimeRemaining.hours + ' hours <br>' +
                       anniversaryTimeRemaining.minutes + ' minutes <br>' +
-                      'and ' + anniversaryTimeRemaining.seconds +' seconds <br>' + 
-                      	'since we have been together :D'
-  						;
+                      'and ' + anniversaryTimeRemaining.seconds +' seconds since we have been together :D';
     if(timeRemaining.total <= 0){
       clearInterval(timeinterval);
     }
   }, 1000);
+
 }
 
 window.onload = function(){  
-
 	initializeClock("clock", finalDate);
+
 }
 
 
